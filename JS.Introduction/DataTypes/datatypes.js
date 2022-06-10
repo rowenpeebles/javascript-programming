bd266112751126b53709fec3c8a071bf84b6219e
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-10 14:56:26 +0100

    Adding JS DataTypes

---
+++ JS.Introduction/DataTypes/datatypes.js
@@ -0,0 +1,27 @@
+// Strings
+const name = "Rowen";
+const height = "6ft";
+console.log(name + ' ' + height);
+
+// Numbers
+const age = 25
+console.log("You are " + age + " years old.");
+
+// Other number types
+const weight = 86.77;
+const number = 3e5 //3*10^5
+
+// Boolean
+const dataChecked = true;
+const valueCounted = false;
+
+// Symbols
+const value1 = Symbol('hello');
+const value2 = Symbol('hello');
+
+// Script Object
+const student {
+    firstName: 'Rowen',
+    lastName: 'Peebles', 
+    classNumber: 10
+};
