516aa042d4e7489f68aff6bc2fbf3c748abfa76f
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-10 13:50:23 +0100

    Adding JS VariablesAndConstants

---
+++ JS.Introduction/VariablesAndConstants/variablesandconstants.js
@@ -0,0 +1,11 @@
+// Declare and initialise variables
+
+const firstName = "Rowen";
+const lastName = "Peebles";
+
+let age = 25;
+let height = "6ft";
+
+console.log("Your name is: " + firstName + " " + lastName);
+console.log("Your age is: " + age);
+console.log("Your height is: " + height);
