88d918a9c6474c8a4c723109fe3be2286dd4b24d
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-14 12:23:51 +0100

    Adding JS Objects

---
+++ JS.Objects/Objects/Example1/Example1.js
@@ -0,0 +1,10 @@
+// Object Methods
+
+const person = {
+    name: 'Sam',
+    age: 20,
+    greet: function() {console.log('hello')}
+}
+
+person.greet();
+console.log(person.name);
---
+++ JS.Objects/Objects/Example2/Example2.js
@@ -0,0 +1,13 @@
+// Nested Objects
+
+const student = {
+    name: 'John',
+    age: 20, 
+    marks: {
+        science: 70,
+        math: 56
+    }
+}
+
+console.log(student.marks);
+console.log(student.marks.math);
