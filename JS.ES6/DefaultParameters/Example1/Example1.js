5938e9c0a11443e56ff38251ecfb8122e7489f01
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-15 13:47:13 +0100

    Adding JS DefaultParameters

---
+++ JS.ES6/DefaultParameters/Example1/Example1.js
@@ -0,0 +1,7 @@
+// Passing parameter as default values
+
+function sum(x = 1, y = x, z = x + y) {
+    console.log(x + y + z);
+}
+
+sum();
---
+++ JS.ES6/DefaultParameters/Example2/Example2.js
@@ -0,0 +1,10 @@
+// Passing function value as default value
+
+const sum = () => 15;
+
+const calculate = function(x, y = x * sum()) {
+    return x + y;
+}
+
+const result = calculate(10);
+console.log(result);
