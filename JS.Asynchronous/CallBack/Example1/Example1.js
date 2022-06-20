781303ef810cb7301566ab4ef89e9b071814678d
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-20 10:15:30 +0100

    Adding JS CallBack

---
+++ JS.Asynchronous/CallBack/Example1/Example1.js
@@ -0,0 +1,12 @@
+// Program with setTimeout()
+
+function greet() {
+    console.log('Hello World');
+}
+
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+setTimeout(greet, 2000);
+sayName('John');
---
+++ JS.Asynchronous/CallBack/Example2/Example2.js
@@ -0,0 +1,12 @@
+// Using a CallBack function
+
+function greet(name, myFunction) {
+    console.log('Hello World');
+    myFunction(name);
+}
+
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+setTimeout(greet, 2000, 'John', sayName);
