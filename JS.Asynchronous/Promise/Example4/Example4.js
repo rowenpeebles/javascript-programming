519dd51a3ef4d2b6d9b8c983f96632b767da8d63
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-20 10:43:56 +0100

    Adding JS Promise

---
+++ JS.Asynchronous/Promise/Example4/Example4.js
@@ -0,0 +1,11 @@
+// JS finally() method
+
+let countValue = new Promise(function(resolve, reject) {
+    resolve('Promise resolved');
+});
+
+countValue.finally(
+    function greet() {
+        console.log('This code is executed');
+    }
+);
