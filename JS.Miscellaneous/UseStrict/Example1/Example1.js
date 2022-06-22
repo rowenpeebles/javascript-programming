c2694a846faa28287c865689d0b7bbf5a6f9dbfb
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-22 12:28:16 +0100

    Adding JS UseStrict

---
+++ JS.Miscellaneous/UseStrict/Example1/Example1.js
@@ -0,0 +1,7 @@
+// Use Strict Mode in Variable
+
+console.log('some code');
+
+// ignored because not at the top
+"use strict";
+ x = 21;
---
+++ JS.Miscellaneous/UseStrict/Example2/Example2.js
@@ -0,0 +1,12 @@
+// Strict Mode in Function
+
+myVar = 9;
+console.log(myVar);
+
+function hello() {
+    // applicable only for this function
+    "use strict";
+    string = 'hello';
+}
+
+hello();
